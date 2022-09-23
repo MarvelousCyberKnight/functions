4c7051277303792df5f4b583165e1b2b3b5572ce
SIDDARDA GOWTHAM <85551731+SIDDARADA@users.noreply.github.com>
2022-09-23 22:18:52 +0530

    Create arith.py

---
+++ arith.py
@@ -0,0 +1,32 @@
+def add(a,b):
+    c=a+b
+    return c
+def sub(a,b):
+    c=a-b
+    return c
+def mul(a,b):
+    c=a*b
+    return c
+def div(a,b):
+    c=a/b
+    return c
+def floor(a,b):
+    c=a//b
+    return c
+def rem(a,b):
+    c=a%b
+    return c
+a=int(input())
+b=int(input())
+d=add(a,b)
+e=sub(a,b)
+f=mul(a,b)
+g=div(a,b)
+h=floor(a,b)
+i=rem(a,b)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
